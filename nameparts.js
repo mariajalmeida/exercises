b928514ed261c6a0795727bd091620ac1a38529f
mariajalmeida <mari9d91@stud.kea.dk>
2021-02-08 15:58:46 +0100

    3 exercise

--- nameparts.js
+++ nameparts.js
@@ -20,31 +20,13 @@
 
 // console.log(finalName + finalPart);
 
+// third exercise
 const name = "peTer heruNIMous liNd";
 
-const firstSpace = name.indexOf(" ");
-const lastSpace = name.lastIndexOf(" ");
+const separateNames = name.split(" ");
 
-const firstLetter = name.charAt(0) + name.charAt(firstSpace + 1) + name.charAt(lastSpace + 1)
-
-const firstLetterUppercase = firstLetter.toUpperCase();
-
-console.log(firstLetterUppercase)
-
-// const firstName = name.substring(0, firstSpace);
-// const findFirstLetter = firstName.substring(0, 1);
-// const firstLetter = findFirstLetter.toUpperCase();
-// const lowerFirstRest = name.substring(1, firstSpace);
-// const firstLoweredName = lowerFirstRest.toLowerCase();
-
-// console.log("first name:", firstLetter + firstLoweredName);
-
-
-// const middleName = name.substring(firstSpace + 1, lastSpace);
-// const findMiddleFirstLetter = middleName.substring(0, 1);
-// const middlefirstLetter = findMiddleFirstLetter.toUpperCase();
-// const lowerSecondRest = name.substring(firstSpace + 1 + 1, lastSpace);
-// const secondLoweredName = lowerSecondRest.toLowerCase();
-
-
-// console.log("second name:", middlefirstLetter + secondLoweredName);
+separateNames.forEach(f => {
+    const firstLetter = f.substring(0, 1).toUpperCase();
+    const restName = f.substring(1).toLowerCase();
+    console.log(firstLetter + restName);
+});
